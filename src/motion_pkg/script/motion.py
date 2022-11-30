a6cbdb67d575c0afb0b744378899dff5dd478a59
Ahmed Hussien <ahmedhussien783@gmail.com>
2022-12-01 00:39:09 +0200

    Motion_pkg:Fix instantaneous stop upon button Release

--- src/motion_pkg/script/motion.py
+++ src/motion_pkg/script/motion.py
@@ -10,19 +10,20 @@ pub = rospy.Publisher("/robot/cmd_vel",Twist,queue_size=10)
 
 def motionKeysPressed(key):
     msg = Twist()
+    velocity = 1
     try:
         char = key.char
         if char == 'w' or char == 'W':
-            msg.linear.x = 0.5
+            msg.linear.x = velocity
             pub.publish(msg)
         elif char == 's' or char == 'S':
-            msg.linear.x = -0.5
+            msg.linear.x = -velocity
             pub.publish(msg)
         elif char == 'a' or char == 'A':
-            msg.angular.z = 0.5
+            msg.angular.z = velocity
             pub.publish(msg)
         elif char == 'd' or char == 'D':
-            msg.angular.z = -0.5
+            msg.angular.z = -velocity
             pub.publish(msg)
         else:
             print("nothing from press")
@@ -30,25 +31,28 @@ def motionKeysPressed(key):
         pass
 
 def motionKeysReleased(key):
-    msg = Twist()
     char = key.char
     try:
         if char == 'w' or char == 'W':
-            msg.linear.x = 0.0
-            pub.publish(msg)
+            rospy.Timer(rospy.Duration(1.0),stopRobot,oneshot=True)
         elif char == 's' or char == 'S':
-            msg.linear.x = 0.0
-            pub.publish(msg)
+            rospy.Timer(rospy.Duration(1.0),stopRobot,oneshot=True)
         elif char == 'a' or char == 'A':
-            msg.angular.z = 0.0
-            pub.publish(msg)
+            rospy.Timer(rospy.Duration(1.0),stopRobot,oneshot=True)
         elif char == 'd' or char == 'D':
-            msg.angular.z = 0.0
-            pub.publish(msg)
+            rospy.Timer(rospy.Duration(1.0),stopRobot,oneshot=True)
         else:
             print("nothing from release")
     except AttributeError:
         pass
+
+
+def stopRobot(e):
+    msg = Twist()
+    msg.linear.x = 0.0
+    msg.angular.z = 0.0
+    pub.publish(msg)
+    
 
 def main():
     start_time = Time.now().to_sec()
