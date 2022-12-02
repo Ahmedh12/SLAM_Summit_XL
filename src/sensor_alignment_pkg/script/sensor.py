811819445ad5bdf779be8ef1cf48a74d2c72d280
Ahmed Hussien <ahmedhussien783@gmail.com>
2022-12-02 16:48:32 +0200

    Sensor_alighnment_pkg : Add Custom message to Publish Aligned Sensor Readings

--- src/sensor_alignment_pkg/script/sensor.py
+++ src/sensor_alignment_pkg/script/sensor.py
@@ -4,8 +4,7 @@ from rospy.rostime import Time
 import message_filters
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
-
-
+from sensor_alignment_pkg.msg import Readings
 
 '''
 /robot/front_laser/scan Type: sensor_msgs/LaserScan 
@@ -18,23 +17,29 @@ from nav_msgs.msg import Odometry
     link : http://docs.ros.org/en/api/nav_msgs/html/msg/Odometry.html
 '''
 
-def OnDataRecieve(frontLaser,rearLaser,odometry):
-    print("Front Laser: ")
-    print(frontLaser)
+pub = rospy.Publisher("sensor_readings",Readings,queue_size=10)
 
+def OnDataRecieved(frontLaser,rearLaser,odometry):
+    #TODO : Fill Readings Message with the appropriate data from Sensor Reading and Odometry 
+    #We can Eding msg/Readings.msg if necessary
+
+    msg = Readings()
+    pub.publish(msg)
+    
 
 def main():
     #intializing Node
     rospy.init_node('sensors_data')
-    
+    first = True
     #subscribing to sensor data topics
     front_laser_sub = message_filters.Subscriber('/robot/front_laser/scan',LaserScan)
     rear_laser_sub = message_filters.Subscriber('/robot/rear_laser/scan',LaserScan)
     odometry_sub = message_filters.Subscriber('/robot/robotnik_base_control/odom',Odometry)
+    
 
     #Aligning Sensor data
-    ts = message_filters.ApproximateTimeSynchronizer([front_laser_sub, rear_laser_sub,odometry_sub],40,0.1,allow_headerless=False)
-    ts.registerCallback(OnDataRecieve)
+    ts = message_filters.ApproximateTimeSynchronizer([front_laser_sub, rear_laser_sub,odometry_sub],40,0.05,allow_headerless=False)
+    ts.registerCallback(OnDataRecieved)
 
     while not rospy.is_shutdown():
         pass
