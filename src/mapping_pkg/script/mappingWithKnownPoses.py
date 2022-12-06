f0b7039de2cd6662c609c098335cf0d4af01e3f4
Ahmed Hussien <ahmedhussien783@gmail.com>
2022-12-07 00:24:52 +0200

    Add mapping Function to compute grid occupancy

--- src/mapping_pkg/script/mappingWithKnownPoses.py
+++ src/mapping_pkg/script/mappingWithKnownPoses.py
@@ -3,15 +3,78 @@ import rospy
 import numpy as np
 from mapping_pkg.msg import Readings
 from nav_msgs.msg import OccupancyGrid
+import math
+ 
+#Global Variables
+cells = np.ones((4992,4992),dtype=np.int8)
+cells = cells * -1 # -1 means unknown, 0 means free, 100 means occupied
+resolution = 0.02 #meters
 
+
+def euler_from_quaternion(x, y, z, w):
+        """
+        Convert a quaternion into euler angles (roll, pitch, yaw)
+        roll is rotation around x in radians (counterclockwise)
+        pitch is rotation around y in radians (counterclockwise)
+        yaw is rotation around z in radians (counterclockwise)
+        """
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return roll_x, pitch_y, yaw_z # in radians
+
+
+
+'''
+Assumptions:
+1- Assume msg.pose is the pose of the robot at the time of the sensor reading relative to the real world frame
+'''
 #TODO: Implement the mapping algorithm Given the sensor readings and the odometry data in the Readings message
 
+
+
+def computeOccupancy(SensorReading):
+    
+    #Robot Pose
+    x = SensorReading.pose.pose.position.x
+    y = SensorReading.pose.pose.position.y
+    theta = euler_from_quaternion(SensorReading.pose.pose.orientation.x,SensorReading.pose.pose.orientation.y,SensorReading.pose.pose.orientation.z,SensorReading.pose.pose.orientation.w)[2]
+    Robotpose = [x,y,theta]
+
+    print("Recived: "+str(SensorReading.pose.pose.position.x))
+
+    #Check for ray end points to mark cells as occupied
+    for i in range(len(SensorReading.ranges)):
+        if SensorReading.ranges[i] != "NaN" and SensorReading.ranges[i] <= SensorReading.range_max and SensorReading.ranges[i] >= SensorReading.range_min:
+            #Compute the end point of the ray 
+            ray_angle = SensorReading.start_angle + (SensorReading.angle_increment*i) + Robotpose[2] + (3*math.pi/4)
+
+            x = SensorReading.ranges[i]*math.cos(ray_angle)+Robotpose[0]
+            y = SensorReading.ranges[i]*math.sin(ray_angle)+Robotpose[1]
+            #Mark the cell as occupied
+            cells[int((x+50)/0.02),int((y+50)/0.02)] = 100
+    
+    #mark the cells that are in the sensor range as free if not occupied
+
+
+
+
+
 def onDataRecived(msg , pub):
-    cells = np.ones((4992,4992),dtype=np.int8)
-    cells = cells * -1 #intially all cells are unknown
     map = OccupancyGrid() #map data to be published
 
-    
+    # print(msg.pose.pose.position)
+
     map.header.frame_id = "robot_map"
     map.info.resolution = 0.02
     map.info.width = 4992
@@ -23,19 +86,18 @@ def onDataRecived(msg , pub):
     map.info.origin.orientation.y = 0
     map.info.origin.orientation.z = 0
     map.info.origin.orientation.w = 1
-
+    computeOccupancy(msg)
     map.data = tuple(cells.flatten())
 
     pub.publish(map)
-
-    pass
+    # print("Published map")
 
 def main():
     #intializing Node
     rospy.init_node('Mapping')
     
     #publish The map data to the topic map_data
-    pub = rospy.Publisher("map_data",OccupancyGrid,queue_size= 10)
+    pub = rospy.Publisher("map_data",OccupancyGrid,queue_size= 1)
 
     #subscribe to the aligned Sensor Readings topic
     rospy.Subscriber("/sensor_readings" , Readings , onDataRecived , (pub))
--- src/mapping_pkg/script/sensorAlignment.py
+++ src/mapping_pkg/script/sensorAlignment.py
@@ -17,11 +17,13 @@ from mapping_pkg.msg import Readings
     link : http://docs.ros.org/en/api/nav_msgs/html/msg/Odometry.html
 '''
 
-pub = rospy.Publisher("sensor_readings",Readings,queue_size=10)
 
 def OnDataRecieved(frontLaser,rearLaser,odometry):
     #Sensor Model Readings
     msg = Readings()
+
+    msg.header.frame_id = odometry.header.frame_id
+
     msg.start_angle = frontLaser.angle_min
     msg.angle_increment = frontLaser.angle_increment
     msg.range_min = frontLaser.range_min
@@ -29,18 +31,29 @@ def OnDataRecieved(frontLaser,rearLaser,odometry):
 
     #all front laser beams plus (310->490) rays from rear laser
     temp_beams_list = list(frontLaser.ranges)
-    temp_beams_list.extend(list(rearLaser.ranges)[310:490:1])
+    temp_beams_list.extend(list(rearLaser.ranges)[180:360:1])
     msg.ranges = tuple(temp_beams_list)
 
     #Motion Model Readings:
     msg.pose = odometry.pose
+    end_time = rospy.Time.now()
 
-    pub.publish(msg)
+    global start_time
+    global pub
+    
+    wait = end_time - start_time
+    if(wait.secs > 3):
+        print("Sent: "+str(odometry.pose.pose.position.x))
+        start_time = rospy.Time.now()
+        pub.publish(msg)
     
 
 def main():
     #intializing Node
     rospy.init_node('sensors_data')
+    global start_time
+    start_time = rospy.Time.now()
+
     #subscribing to sensor data topics
     front_laser_sub = message_filters.Subscriber('/robot/front_laser/scan',LaserScan)
     rear_laser_sub = message_filters.Subscriber('/robot/rear_laser/scan',LaserScan)
@@ -48,8 +61,12 @@ def main():
     
 
     #Aligning Sensor data
-    ts = message_filters.ApproximateTimeSynchronizer([front_laser_sub, rear_laser_sub,odometry_sub],40,0.05,allow_headerless=False)
+    ts = message_filters.ApproximateTimeSynchronizer([front_laser_sub, rear_laser_sub,odometry_sub],1,1,allow_headerless=False,reset=True)
     ts.registerCallback(OnDataRecieved)
+
+    #Publishing Sensor data
+    global pub
+    pub = rospy.Publisher("sensor_readings",Readings,queue_size=1)
 
     while not rospy.is_shutdown():
         pass
