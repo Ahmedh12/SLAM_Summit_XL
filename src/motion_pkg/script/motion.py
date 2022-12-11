6d55730fd0cd5d23cc138fde7cbccdaccdf7e434
nadaabdelgayed <75578900+nadaabdelgayed@users.noreply.github.com>
2022-12-11 04:41:38 +0200

    updated robot motion module
    added the acceleration and deceleration features.
    added multiple key presses feature.

--- src/motion_pkg/script/motion.py
+++ src/motion_pkg/script/motion.py
@@ -4,62 +4,101 @@ from rospy.rostime import Time
 from geometry_msgs.msg import Twist
 from pynput.keyboard import Key, Listener,KeyCode
 
-
-rospy.init_node("cust_motion_controller")
-pub = rospy.Publisher("/robot/cmd_vel",Twist,queue_size=10)
-
-def motionKeysPressed(key):
+class robotMotion:
+    leftKey=False
+    rightKey=False
+    upKey=False
+    downKey=False
+    rospy.init_node("cust_motion_controller")
+    pub = rospy.Publisher("/robot/robotnik_base_control/cmd_vel",Twist,queue_size=10)
     msg = Twist()
-    velocity = 1
-    try:
-        char = key.char
-        if char == 'w' or char == 'W':
-            msg.linear.x = velocity
-            pub.publish(msg)
-        elif char == 's' or char == 'S':
-            msg.linear.x = -velocity
-            pub.publish(msg)
-        elif char == 'd' or char == 'D':
-            msg.angular.z = -velocity
-            pub.publish(msg)
-        elif char == 'a' or char == 'A':
-            msg.angular.z = velocity
-            pub.publish(msg)
-        else:
+    velocity_step = 0.1
+    def keyPress(self,key):
+        # monitoring key presses to capture multiple inputs
+        value=key.char.lower()
+        if value == 'w':
+            self.upKey=True
+        elif value == 'a':
+            self.leftKey=True
+        elif value == 'd':
+            self.rightKey=True
+        elif value == 's':
+            self.downKey=True
+
+    def keyRelease(self,key):
+        # monitoring key releases 
+        value=key.char.lower()
+        if value == 'w':
+            self.upKey=False
+        elif value == 'a':
+            self.leftKey=False
+        elif value == 'd':
+            self.rightKey=False
+        elif value == 's':
+            self.downKey=False
+    
+    def moveRobot(self):
+        max_velocity = 2
+        # acceelerating the robot according to the pressed keys
+        try:            
+            if self.upKey:
+                if self.msg.linear.x < max_velocity:
+                    self.msg.linear.x += self.velocity_step
+
+            if self.downKey:
+                if self.msg.linear.x > -max_velocity:
+                    self.msg.linear.x -= self.velocity_step
+    
+            if self.rightKey:
+                if self.msg.angular.z > -max_velocity:
+                    self.msg.angular.z -= self.velocity_step
+
+            if self.leftKey:
+                if self.msg.angular.z < max_velocity:
+                    self.msg.angular.z += self.velocity_step
+
+            self.pub.publish(self.msg)
+            
+        except AttributeError:
             pass
-    except AttributeError:
-        pass
+   
+    def stopRobot(self):
+        # decceelerating the robot according to the released keys
+        if not self.downKey and round(self.msg.linear.x,1)<0:
+            self.msg.linear.x=round(self.msg.linear.x,1)+self.velocity_step
 
-def motionKeysReleased(key):
-    char = key.char
-    try:
-        if char == 'w' or char == 'W':
-            rospy.Timer(rospy.Duration(1.0),stopRobot,oneshot=True)
-        elif char == 's' or char == 'S':
-            rospy.Timer(rospy.Duration(1.0),stopRobot,oneshot=True)
-        elif char == 'a' or char == 'A':
-            rospy.Timer(rospy.Duration(1.0),stopRobot,oneshot=True)
-        elif char == 'd' or char == 'D':
-            rospy.Timer(rospy.Duration(1.0),stopRobot,oneshot=True)
-        else:
-            pass
-    except AttributeError:
-        pass
+        if not self.upKey and round(self.msg.linear.x,1)>0:
+            self.msg.linear.x=round(self.msg.linear.x,1)-self.velocity_step
+
+        if not self.rightKey and round(self.msg.angular.z,1)<0:
+            self.msg.angular.z=round(self.msg.angular.z,1)+self.velocity_step
+
+        if not self.leftKey and round(self.msg.angular.z,1)>0:
+            self.msg.angular.z=round(self.msg.angular.z,1)-self.velocity_step
+
+        if round(self.msg.linear.x,1) == 0 and round(self.msg.angular.z,1) == 0:
+            self.msg.linear.x=0.0
+            self.msg.angular.z=0.0
+
+        self.pub.publish(self.msg)
 
 
-def stopRobot(e):
-    msg = Twist()
-    msg.linear.x = 0.0
-    msg.angular.z = 0.0
-    pub.publish(msg)
+
+
+
+
+
     
 
 def main():
-    listner =Listener(on_press=motionKeysPressed,
-                      on_release=motionKeysReleased)
+    RM = robotMotion()
+    listner =Listener(on_press=RM.keyPress,
+                      on_release=RM.keyRelease)
     listner.start()
-
     while not rospy.is_shutdown():
+        RM.moveRobot()
+        RM.stopRobot()
+        rospy.sleep(0.05)
         pass
 
 
